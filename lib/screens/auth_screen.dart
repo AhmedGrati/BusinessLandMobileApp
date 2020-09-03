0b40b1b521f2e0378fc8f1e6fb4f355bc30ed174
mohamedwaelbenismail <ahmedgrati1999@gmail.com>
2020-09-03 11:03:17 +0100

    add animations

--- lib/screens/auth_screen.dart
+++ lib/screens/auth_screen.dart
@@ -1,16 +1,50 @@
 
+import 'package:businessland_app/screens/auth_stage.dart';
+import 'package:businessland_app/screens/components/card_fade_in_animation.dart';
+import 'package:businessland_app/screens/components/login_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 import '../size_config.dart';
 import 'components/register_widget.dart';
 
-class AuthScreen extends StatelessWidget {
+class AuthScreen extends StatefulWidget {
+  @override
+  _AuthScreenState createState() => _AuthScreenState();
+}
+
+class _AuthScreenState extends State<AuthScreen> with SingleTickerProviderStateMixin {
+  AuthStage stage = AuthStage.Login;
+  AnimationController _controller;
+  Animation _animation;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      vsync: this,
+      duration: Duration(seconds: 2)
+    );
+    print(_controller);
+    _animation = Tween(
+      begin: 0.0,
+      end: 1.0
+    ).animate(_controller);
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _controller.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
+
+    // initiliazing the size config
     SizeConfig().init(context);
     double defaultSize = SizeConfig.defaultSize;
-    print(defaultSize);
+    _controller.forward();
     return SingleChildScrollView(
       child:Container(
         padding: EdgeInsets.all(16),
@@ -36,11 +70,11 @@ class AuthScreen extends StatelessWidget {
                   fontWeight: FontWeight.w500,
               ),
             ),
-            Text('vikinger'.toUpperCase() ,
+            Text('businessland'.toUpperCase() ,
               style: GoogleFonts.titilliumWeb(
                   color: Colors.white,
                   fontWeight: FontWeight.w900,
-                  fontSize: 50
+                  fontSize: 40
               ),
             ),
             Row(
@@ -49,7 +83,11 @@ class AuthScreen extends StatelessWidget {
                   Expanded(
                     child: Container(
                       child: FlatButton(
-                        onPressed: () {},
+                        onPressed: () {
+                          setState(() {
+                            this.stage = AuthStage.Register;
+                          });
+                        },
                         color: Color(0xff1D2333),
                         child: Padding(
                           padding: const EdgeInsets.all(16.0),
@@ -66,9 +104,13 @@ class AuthScreen extends StatelessWidget {
                     ),
                   ),
                   Expanded(
-                    child: Container( 
+                    child: Container(
                       child: FlatButton(
-                        onPressed: () {},
+                        onPressed: () {
+                          setState(() {
+                            this.stage = AuthStage.Login;
+                          });
+                        },
                         color: Color(0xffffffff),
                         child: Padding(
                           padding: const EdgeInsets.all(16.0),
@@ -111,7 +153,7 @@ class AuthScreen extends StatelessWidget {
                   ),
                 ),
                 Expanded(
-                  child: Container( 
+                  child: Container(
                     child: FlatButton(
                       onPressed: () {},
                       color: Color(0xffffffff),
@@ -133,7 +175,16 @@ class AuthScreen extends StatelessWidget {
             SizedBox(
               height : defaultSize
             ),
-            RegisterWidget(),
+            stage == AuthStage.Login
+                ? FadeTransition(
+              opacity: _animation,
+              child: LoginWidget(),
+            ) :
+                  (stage == AuthStage.Register
+                      ? FadeTransition(
+                    opacity: _animation,
+                    child: RegisterWidget(),
+                  ) : null)// todo add Forgot password widget
           ],
         ),
       )
--- lib/size_config.dart
+++ lib/size_config.dart
@@ -7,12 +7,13 @@ class SizeConfig {
   static double screenHeight;
   static double defaultSize;
   static Orientation orientation;
-
+  static bool disableAnimation;
   void init(BuildContext context) {
     _mediaQueryData = MediaQuery.of(context);
     screenWidth = _mediaQueryData.size.width;
     screenHeight = _mediaQueryData.size.height;
     orientation = _mediaQueryData.orientation;
+    disableAnimation = _mediaQueryData.disableAnimations;
     // So if the screen size increase or decrease then our defaultSize also vary
     defaultSize = orientation == Orientation.landscape
         ? screenHeight * 0.024
